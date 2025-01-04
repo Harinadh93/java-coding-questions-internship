072a43b086d17207cf6d747f541285ea04936004
harinadh93 <harinadhbabueluru.com>
2025-01-04 16:54:11 +0530

    Java Coding Questions - Internship

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,20 @@
+package javaCodingQuestions;
+
+public class SumOfTwoNumbers {
+
+	static int a = 10;
+	static int b = 20;
+	static int c = a+b;
+	
+	static void sum()
+	{
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args) {
+		
+		sum();
+
+	}
+
+}
